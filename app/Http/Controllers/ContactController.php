a1df0b1d9c674fd9599e6b2feb7424eee706afde
Dev004-binarybrix <153667660+Dev004-binarybrix@users.noreply.github.com>
2024-08-02 14:44:06 +0500

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -22,7 +22,7 @@ class ContactController extends Controller
         ContactInfo::create($data);
 
         try {
-            Mail::to('ummyhabiba999@gmail.com')->send(new ContactMail($data));
+            Mail::to('info@binarybrix.com')->send(new ContactMail($data));
             return response()->json(['success' => true, 'message' => 'Thankyou for contacting us!']);
         } catch (\Exception $e) {
             return response()->json(['success' => false, 'message' => 'Failed to send message.'], 500);
