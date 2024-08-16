b4c6e7e3ab1f65a5e6665f90e819d04578092571
Dev004-binarybrix <153667660+Dev004-binarybrix@users.noreply.github.com>
2024-08-16 11:55:17 +0500

    update nav bar

--- resources/views/includes/website/navbar.blade.php
+++ resources/views/includes/website/navbar.blade.php
@@ -21,96 +21,94 @@
 
     <div class="branding d-flex align-items-center">
 
-        <div class="container position-relative d-flex align-items-center justify-content-between">
-            <a href="{{ route('home') }}" class="logo d-flex align-items-center">
-                <img src="./assets/img/brixLogo.png">
-            </a>
+        <div class="container-fluid position-relative d-flex justify-content-between align-items-center">
+            <div class="ms-3">
+                <a href="{{ route('home') }}" class="logo d-flex align-items-center">
+                    <img src="./assets/img/brixLogo.png">
+                </a>
+            </div>
+<div class="margin-right ">
+    <nav id="navmenu" class="navmenu">
+        <ul>
+            <li>
+                <a href="https://www.facebook.com/binarybrix" target="_blank">
+                    <img src="./assets/img/facebook.png" alt="Facebook" style="width: 40px; height: 40px; tool">
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/binarybrix/" target="_blank">
+                    <img src="./assets/img/instagram.png" alt="Instagram" style="width: 45px; height: 45px;">
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/company/binarybrix/mycompany/" target="_blank">
+                    <img src="./assets/img/linkedin.png" alt="Linked In" style="width: 40px; height: 40px;">
+                </a>
+            </li>
+            <li>
+                <a href="https://wa.me/923122166633" target="_blank">
+                    <img src="./assets/img/whatsapp.png" alt="Whatsapp" style="width: 40px; height: 40px">
+                </a>
+            </li>
 
-            <nav id="navmenu" class="navmenu">
-                <ul>
-                    <li>
-                        <a href="https://www.facebook.com/binarybrix" target="_blank">
-                            <img src="./assets/img/facebook.png" alt="Facebook" style="width: 40px; height: 40px; tool">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/binarybrix/" target="_blank">
-                            <img src="./assets/img/instagram.png" alt="Instagram" style="width: 45px; height: 45px;">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/company/binarybrix/mycompany/" target="_blank">
-                            <img src="./assets/img/linkedin.png" alt="Linked In" style="width: 40px; height: 40px;">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://wa.me/923122166633" target="_blank">
-                            <img src="./assets/img/whatsapp.png" alt="Whatsapp" style="width: 40px; height: 40px">
-                        </a>
-                    </li>
-
-                    <li><a href="{{ url('/') }}" class="active">Home</a></li>
-                    <li class="dropdown"><a href="{{ url('/services') }}"><span>Services</span> <i
-                                class="bi bi-chevron-down toggle-dropdown"></i></a>
-                        <ul>
-                            <li class="nav-item"><a href="{{ route('custom-software') }}"><i class="bi bi-gear"></i>
-                                    Custom Software Development</a></li>
-                            <li class="nav-item"><a href="{{ route('web-application') }}"><i
-                                        class="bi bi-browser-chrome"></i> Web Application Development</a></li>
-                            <li class="nav-item"><a href="{{ route('mobile-application') }}"><i class="bi bi-phone"></i>
-                                    Mobile Application Development</a></li>
-                            <li class="nav-item"><a href="{{ route('high-tech-game') }}"><i
-                                        class="bi bi-controller"></i> High-Tech Game Development</a></li>
-                            <li class="nav-item"><a href="{{ route('cloud-based') }}"><i class="bi bi-cloud"></i>
-                                    Cloud-Based Development</a></li>
-                            <li class="nav-item"><a href="{{ route('dev-ops') }}"><i class="bi bi-tools"></i> DevOps
-                                    Development</a></li>
-                            <li class="nav-item"><a href="{{ route('e-commerce') }}"><i class="bi bi-cart"></i>
-                                    E-commerce Development</a></li>
-                            <li class="nav-item"><a href="{{ route('ui-ux-design') }}"><i class="bi bi-palette"></i>
-                                    UI/UX Design and Development</a></li>
-                            <li class="nav-item"><a href="{{ route('iot-development') }}"><i class="bi bi-wifi"></i>
-                                    IoT Development</a></li>
-                            <li class="nav-item"><a href="{{ route('machine-learning') }}"><i class="bi bi-robot"></i>
-                                    Artificial Intelligence & Machine Learning</a></li>
-                            <li class="nav-item"><a href="{{ route('expert-qa-testing') }}"><i
-                                        class="bi bi-check-circle"></i> Expert QA Testing</a></li>
-
-                        </ul>
-
-
-                    </li>
-                    <li ><a href="{{ url('/') }}#portfolio">Portfolio</a></li>
-                    <li><a href="{{ url('/') }}#contact">Contact</a></li>
-
-                    <li class="dropdown"><a href="{{ url('/') }}#about"><span>About Us</span> <i
+            <li><a href="{{ url('/') }}" class="active">Home</a></li>
+            <li class="dropdown"><a href="{{ url('/services') }}"><span>Services</span> <i
                         class="bi bi-chevron-down toggle-dropdown"></i></a>
                 <ul>
-                    <li class="nav-item"><a href="{{ url('/') }}#about"><i class="bi bi-info-circle"></i>
-                            About Us</a></li>
-                    <li class="nav-item"><a href="{{ url('/') }}#testimonials"><i
-                                class="bi bi-stars"></i>Testimonial</a></li>
-
-                    <li class="nav-item"><a href="{{ url('/') }}#team"><i
-                                class="bi bi-people"></i> BinaryBrix Team</a></li>
-                                <li class="nav-item"><a href="{{ url('/') }}#contact"><i class="bi bi-envelope"></i>Contact Us</a></li>
-                    <li class="nav-item"><a href="{{ url('/') }}"><i class="bi bi-briefcase"></i>Career</a></li>
-                    <li class="nav-item"><a href="{{ url('/') }}#faq"><i class="bi bi-question-circle"></i>
-                        F.A.Q</a></li>
-
-
-
+                    <li class="nav-item"><a href="{{ route('custom-software') }}"><i class="bi bi-gear"></i>
+                            Custom Software Development</a></li>
+                    <li class="nav-item"><a href="{{ route('web-application') }}"><i
+                                class="bi bi-browser-chrome"></i> Web Application Development</a></li>
+                    <li class="nav-item"><a href="{{ route('mobile-application') }}"><i class="bi bi-phone"></i>
+                            Mobile Application Development</a></li>
+                    <li class="nav-item"><a href="{{ route('high-tech-game') }}"><i
+                                class="bi bi-controller"></i> High-Tech Game Development</a></li>
+                    <li class="nav-item"><a href="{{ route('cloud-based') }}"><i class="bi bi-cloud"></i>
+                            Cloud-Based Development</a></li>
+                    <li class="nav-item"><a href="{{ route('dev-ops') }}"><i class="bi bi-tools"></i> DevOps
+                            Development</a></li>
+                    <li class="nav-item"><a href="{{ route('e-commerce') }}"><i class="bi bi-cart"></i>
+                            E-commerce Development</a></li>
+                    <li class="nav-item"><a href="{{ route('ui-ux-design') }}"><i class="bi bi-palette"></i>
+                            UI/UX Design and Development</a></li>
+                    <li class="nav-item"><a href="{{ route('iot-development') }}"><i class="bi bi-wifi"></i>
+                            IoT Development</a></li>
+                    <li class="nav-item"><a href="{{ route('machine-learning') }}"><i class="bi bi-robot"></i>
+                            Artificial Intelligence & Machine Learning</a></li>
+                    <li class="nav-item"><a href="{{ route('expert-qa-testing') }}"><i
+                                class="bi bi-check-circle"></i> Expert QA Testing</a></li>
 
                 </ul>
+            </li>
+            <li ><a href="{{ url('/') }}#portfolio">Portfolio</a></li>
+            <li><a href="{{ url('/') }}#contact">Contact</a></li>
 
-                    </li>
+            <li class="dropdown"><a href="{{ url('/') }}#about"><span>About Us</span> <i
+                class="bi bi-chevron-down toggle-dropdown"></i></a>
+        <ul>
+            <li class="nav-item"><a href="{{ url('/') }}#about"><i class="bi bi-info-circle"></i>
+                    About Us</a></li>
+            <li class="nav-item"><a href="{{ url('/') }}#testimonials"><i
+                        class="bi bi-stars"></i>Testimonial</a></li>
 
-                    <li><a href="{{ url('/') }}#team">Team</a></li>
+            <li class="nav-item"><a href="{{ url('/') }}#team"><i
+                        class="bi bi-people"></i> BinaryBrix Team</a></li>
+                        <li class="nav-item"><a href="{{ url('/') }}#contact"><i class="bi bi-envelope"></i>Contact Us</a></li>
+            <li class="nav-item"><a href="{{ url('/') }}"><i class="bi bi-briefcase"></i>Career</a></li>
+            <li class="nav-item"><a href="{{ url('/') }}#faq"><i class="bi bi-question-circle"></i>
+                F.A.Q</a></li>
+        </ul>
+
+            </li>
+
+            <li><a href="{{ url('/') }}#team">Team</a></li>
 
 
-                </ul>
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+    </nav>
+
+</div>
 
         </div>
 
